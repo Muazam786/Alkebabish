98717fdc4fb211771e1f4dd2c3613d2e9cad693a
Muazam Iftikhar <muazamiftikhar786@gmail.com>
2025-04-11 12:26:27 +0500

    customers.js

---
+++ frontend/customers.js
@@ -0,0 +1,25 @@
+const menu = [
+    { id: 1, category: "Traditional Dishes", name: "Chicken Curry", price: 13.00 },
+    { id: 2, category: "Traditional Dishes", name: "Prawn Curry", price: 15.00 },
+    { id: 3, category: "Traditional Dishes", name: "Lamb Curry", price: 14.00 },
+    { id: 4, category: "Traditional Dishes", name: "Vegetable Curry", price: 12.00 },
+    { id: 5, category: "Vegetarian", name: "Aloo Chana", price: 12.00 },
+    { id: 6, category: "Vegetarian", name: "Spicy Potato Curry", price: 12.00 },
+    { id: 7, category: "Vegetarian", name: "Paneer Curry", price: 12.00 },
+    { id: 8, category: "Vegetarian", name: "Tarka Dal", price: 12.00 },
+    { id: 9, category: "Biryani", name: "Vegetable Biryani", price: 12.00 },
+    { id: 10, category: "Biryani", name: "Chicken Biryani", price: 13.00 },
+    { id: 11, category: "Biryani", name: "Lamb Biryani", price: 14.00 },
+    { id: 12, category: "Biryani", name: "King Prawn Biryani", price: 15.00 },
+    { id: 13, category: "Rice", name: "Boiled Rice", price: 3.50 },
+    { id: 14, category: "Rice", name: "Pilau Rice", price: 4.00 },
+    { id: 15, category: "Rice", name: "Mushroom Pilau Rice", price: 4.00 },
+    { id: 16, category: "Bread", name: "Chapati", price: 2.00 },
+    { id: 17, category: "Bread", name: "Garlic Naan", price: 3.00 },
+    { id: 18, category: "Bread", name: "Cheese Naan", price: 3.50 },
+    { id: 19, category: "Pizza", name: "10” Pizza + Chips & Drink", price: 11.00 },
+    { id: 20, category: "Pizza", name: "12” Pizza + Chips & Drink", price: 13.00 },
+    { id: 21, category: "Kids Meal", name: "Kids Pizza Meal", price: 7.00 },
+    { id: 22, category: "Kids Meal", name: "Kids Nuggets Meal", price: 4.99 }
+  ];
+  
